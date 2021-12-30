c3e2af55190847a8943c85671b6184a21371c747
Tim.P <51896856+brainiac19@users.noreply.github.com>
2021-12-30 22:14:25 +0800

    performance fix

--- RepeatsMarker.py
+++ RepeatsMarker.py
@@ -1,31 +1,39 @@
-import io
 import tkinter as tk
+from collections import defaultdict
 from tkinter import filedialog
-import numpy as np
 
-class RepeatsMarker:
+
+class FileLoader:
     def __init__(self):
-        self.ignore_lines_with_less_than_characters = 10
-        self.show_first_last_lines_count = 3
-        self.ignore_blocks_with_less_than_lines = 3
         self.encoding_try_list = ["utf-8", "utf-16", "gbk", "big5"]
 
-    def get_file_path(self) -> str:
+    @staticmethod
+    def prompt_get_file_path() -> str:
         root = tk.Tk()
         root.withdraw()
         return filedialog.askopenfilename()
-    
-    def read_text_lines(self, file_path: str) -> np.ndarray:
+
+    def read_lines_at_path(self, file_path) -> list:
         for encoding in self.encoding_try_list:
             try:
-                file = open(file_path, encoding=encoding)
-                return np.array(file.readlines())
+                with open(file_path, encoding=encoding) as file:
+                    return file.readlines()
             except UnicodeDecodeError:
                 continue
         else:
             raise Exception("didn't find a matched encoding")
 
-    def calculate_diffs(self, l: list or tuple):
+    def choose_file_read_lines(self):
+        file_path = self.prompt_get_file_path()
+        if file_path == "":
+            return None
+        return self.read_lines_at_path(file_path)
+
+
+class RepeatsMarker:
+
+    @staticmethod
+    def calculate_diffs(l):
         if len(l) < 2:
             raise Exception("list needs to be longer than 2 to calculate diff")
         temp_list = []
@@ -33,84 +41,84 @@ class RepeatsMarker:
             temp_list.append((l[i] - l[i - 1]))
         return temp_list
 
-    def find_earliest_occurrence_key(self, dictionary: dict):
-        return min(dictionary, key=lambda x: dictionary.get(x)[0])
-    
-    def find_repeated_lines_with_indices(self, lines_array:np.ndarray) -> dict:
-        unique_lines = np.unique(lines_array)
-        line_indices_dict = {}
-        for line in unique_lines:
-            if str(line).isspace() or len(line) < self.ignore_lines_with_less_than_characters:
-                continue
-            indices = np.where(lines_array == line)[0]
-            if len(indices) > 1:
-                line_indices_dict[line] = tuple(indices)
-        return line_indices_dict
-    
-    def locate_repeated_blocks(self, repeat_indices_dict:dict) -> list:
-        result_list = []
-        while len(repeat_indices_dict) > 0:
-            base_key = self.find_earliest_occurrence_key(repeat_indices_dict)
-            base_value = repeat_indices_dict.pop(base_key)
-            base_diff = self.calculate_diffs(base_value)
-            delta_temp = 1
+    @staticmethod
+    def find_repeated_lines_with_indices(lines_iterable, ignore_lines_with_less_than_characters = 3, ignore_blank_lines = True):
+        duplicates_dict = defaultdict(list)
+        for i, item in enumerate(lines_iterable):
+            duplicates_dict[item].append(i)
+        return ((key, indices) for key, indices in duplicates_dict.items() if len(indices) > 1 and len(key) >= ignore_lines_with_less_than_characters and (not ignore_blank_lines or not str(key).isspace()))
 
-            while len(repeat_indices_dict) > 0:
-                key = self.find_earliest_occurrence_key(repeat_indices_dict)
-                if self.calculate_diffs(repeat_indices_dict[key]) == base_diff and repeat_indices_dict[key][0] == base_value[
-                    0] + delta_temp:
-                    repeat_indices_dict.pop(key)
-                    delta_temp += 1
-                else:
-                    break
+    @staticmethod
+    def locate_repeated_blocks(repeats_with_indices_iterable, ignore_blocks_with_less_than_lines = 3) -> list:
+        temp_list = []
+        line_index_temp = -2
+        diffs_temp = []
+        for line_with_indices in repeats_with_indices_iterable:
+            diffs = RepeatsMarker.calculate_diffs(line_with_indices[1])
+            if not diffs == diffs_temp or not line_with_indices[1][0] == line_index_temp + 1:
+                diffs_temp = diffs
+                temp_list.append([[index, index] for index in line_with_indices[1]])
 
-            if delta_temp >= self.ignore_blocks_with_less_than_lines:
-                result_list.append([[index, index + delta_temp] for index in base_value])
+            line_index_temp = line_with_indices[1][0]
+            for i, item in enumerate(line_with_indices[1]):
+                temp_list[len(temp_list) - 1][i][1] = item
 
-        return result_list
+        #filter blocks with too few lines
+        range_list = []
+        for _range in temp_list:
+            if RepeatsMarker.calculate_diffs(_range[0])[0] >= ignore_blocks_with_less_than_lines - 1:
+                range_list.append(_range)
 
-    def generate_text_block_to_index_range_dict(self, result_list:list, file_lines_np_arr:np.ndarray) -> dict:
+        return range_list
+
+    @staticmethod
+    def generate_readable_dict(repeated_blocks_list:list, file_lines_list:list, show_first_last_lines_count = 3, index_increment = 1) -> dict:
         result_dict = {}
-        for ls in result_list:
+        for blocks_range_list in repeated_blocks_list:
             key = "\n"
-            if ls[0][1] - ls[0][0] > 6:
-                for i in range(self.show_first_last_lines_count):
-                    key += str(file_lines_np_arr[ls[0][0] + i]).strip("\n")
+            if blocks_range_list[0][1] - blocks_range_list[0][0] > show_first_last_lines_count * 2:
+                for i in range(show_first_last_lines_count):
+                    key += str(file_lines_list[blocks_range_list[0][0] + i]).strip("\n") + "\n"
 
-                key += "\n......\n"
-                for i in range(-self.show_first_last_lines_count + 1, 1):
-                    key += str(file_lines_np_arr[ls[0][1] + i]).strip("\n")
+                key += "\n......\n\n"
+
+                for i in range(-show_first_last_lines_count , 0):
+                    key += str(file_lines_list[blocks_range_list[0][1] + i]).strip("\n") + "\n"
 
             else:
-                for i in range(ls[0][0], ls[0][1]):
-                    key += file_lines_np_arr[i]
+                for i in range(blocks_range_list[0][0], blocks_range_list[0][1] + 1):
+                    key += file_lines_list[i]
             key += "\n\n\n"
-            result_dict[key] = ls
-        return  result_dict
+            result_dict[key] = list([[index + index_increment for index in _range] for _range in blocks_range_list])
+        return result_dict
 
-    def generate_readable_string(self, index_range_dict:dict) -> str:
+    @staticmethod
+    def generate_readable_string(index_range_dict: dict) -> str:
         readable_string = ""
         for key in index_range_dict.keys().__reversed__():
             readable_string += str(index_range_dict[key])
             readable_string += str(key)
         return readable_string
 
-    def main(self):
-        file_path = self.get_file_path()
-        if file_path == "":
-            return
-        file_lines_np_array = self.read_text_lines(file_path)
+    @staticmethod
+    def find_repeats(file_lines:list):
+        repeated_lines_with_range = RepeatsMarker.find_repeated_lines_with_indices(file_lines)
+        return RepeatsMarker.locate_repeated_blocks(repeated_lines_with_range)
 
-        line_indices_dict = self.find_repeated_lines_with_indices(file_lines_np_array)
-        repeated_blocks = self.locate_repeated_blocks(line_indices_dict)
-        text_to_index_range_dict = self.generate_text_block_to_index_range_dict(repeated_blocks, file_lines_np_array)
-        readable_string = self.generate_readable_string(text_to_index_range_dict)
 
-        writer = open("check results.txt", "w")
-        writer.write(readable_string)
+def usage_example():
+    file_lines = FileLoader().choose_file_read_lines()
+    if file_lines is None:
+        return
+
+    repeated_blocks = RepeatsMarker.find_repeats(file_lines)
+    readable_dict = RepeatsMarker.generate_readable_dict(repeated_blocks, file_lines)
+    readable_string = RepeatsMarker.generate_readable_string(readable_dict)
+
+    with open("check results.txt", "w") as writer:
+        writer.write("no repeats found" if readable_string == "" else readable_string)
+
 
 if __name__ == "__main__":
-    rm = RepeatsMarker()
-    #set custom parameters, or leave it default
-    rm.main()
+    usage_example()
 
